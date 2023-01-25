2b1654f9a4881c8fbbbc47306d45a64fe64017c6
Josuel2020 <58760738+Josuel2020@users.noreply.github.com>
2023-01-25 15:16:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ from binance.client import Client
 from keys import api_key, api_secret
 
 client = Client(api_key, api_secret)
+
+
+status = client.get_account_status()
+print(status)
+
+# to get account's informations, use:
+info = client.get_account()
+#for item in info:
+#    print(item)
+
+# to see avaible's balance in account, use:
+#print(info["balances"])
+
+
+
+for ativo in info["balances"]:
+    if float(ativo["free"]) > 0:
+        print(ativo)
