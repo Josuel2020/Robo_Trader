d2eee4eb33d358f51ca6b62bc2efcf2370a74e12
Josuel2020 <58760738+Josuel2020@users.noreply.github.com>
2023-01-26 00:52:13 -0300

    orders

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from binance.client import Client
+from binance.enums import *
 from keys import api_key, api_secret
 
 client = Client(api_key, api_secret)
 
 
-status = client.get_account_status()
-print(status)
 
-# to get account's informations, use:
+
+# getting informations of account
 info = client.get_account()
-#for item in info:
-#    print(item)
 
-# to see avaible's balance in account, use:
-#print(info["balances"])
-
-
-
-for ativo in info["balances"]:
+# getting the balances of account
+list_balance = info["balances"]
+for ativo in list_balance:
     if float(ativo["free"]) > 0:
         print(ativo)
+
+#creating an order
+#print(list_balance["asset"])
+
+
+order = client.create_order(
+    symbol='BTCBRL',
+    side=SIDE_BUY,
+    type=ORDER_TYPE_LIMIT,
+    timeInForce=TIME_IN_FORCE_GTC,
+    transactTime=1000,
+    quantity = 0.1,
+    price = 100,
+    )
+
+print(order)
